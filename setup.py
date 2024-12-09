9b707afc983d976abb930d46c826c29bf39c9b9d
Tyler Blume <t-blume@hotmail.com>
2024-12-08 21:10:13 -0500

    init push

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SOARegression",
+    version="0.0.1",
+    author="Tyler Blume",
+    url="https://github.com/tblume1992/SOARegression",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description = "Sample Optimized Adaptive Regression.",
+    author_email = 't-blume@hotmail.com', 
+    keywords = ['machine learning', 'linear regression'],
+      install_requires=[           
+                        'numpy',
+                        'pandas',
+                        'scikit-learn',
+                        'scipy',
+                        'matplotlib',
+                        ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
